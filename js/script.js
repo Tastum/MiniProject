972f3399c5ce3fb38bd2293ba13a53d793edf4de
Magnus Bachalarz <magnus@bachalarz.dk>
2016-05-30 13:51:04 +0200

    no message

--- js/script.js
+++ js/script.js
@@ -11,6 +11,21 @@ function init(){
     createjs.Ticker.setFPS(30);
     createjs.Ticker.addEventListener("tick", tock);
 
+    queue = new createjs.LoadQueue(true);
+    preloadText = new createjs.Text("", "50px Courier New", "#000");
+    queue.installPlugin(createjs.Sound)
+    queue.on("progress", queueProgress);
+    queue.on("complete", queueComplete);
+    queue.loadManifest([
+        //"img/sprites/tiles.png",
+        //"img/hero.png",
+        //"img/key.png",
+        //{id:"tileSprites",src:"json/bgtiles.json"},
+        //{id:"levelJson",src:"json/levels/levels.json"},
+        {id:"bgsound", src:"sounds/bass.mp3"},
+
+    ])
+
     hero = new createjs.Shape();
     hero.graphics.beginFill("blue");
     hero.graphics.drawCircle(0, 0, 40);
@@ -21,6 +36,13 @@ function init(){
     stage.addChild(hero);
 
 }
+
+function queueProgress(e){
+    preloadText.text= Math.round(e.progress*100)+"%"
+    stage.update();
+}
+
+function queueComplete(){}
 
 
 
