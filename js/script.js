1c93ea362768219c0baabeb81e0513eda9d7521a
Magnus Bachalarz <magnus@bachalarz.dk>
2016-05-30 13:33:40 +0200

    test

--- js/script.js
+++ js/script.js
@@ -1,15 +1,25 @@
 var stage
 var hero
 var life
-var level
+var level = 1
 var time
-var co2Niveau
+var co2Niveau = 0
 
 
 function init(){
     stage = new createjs.Stage("canvas");
     createjs.Ticker.setFPS(30);
     createjs.Ticker.addEventListener("tick", tock);
+
+    hero = new createjs.Shape();
+    hero.graphics.beginFill("blue");
+    hero.graphics.drawCircle(0, 0, 40);
+    hero.x = 50;
+    hero.y = 50;
+
+
+    stage.addChild(hero);
+
 }
 
 
