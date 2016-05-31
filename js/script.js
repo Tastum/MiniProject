38fa9c90a1696cb2e14a24a8a117c7740d5bb5af
Magnus Bachalarz <magnus@bachalarz.dk>
2016-05-31 09:21:16 +0200

    Preload tilføjet

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+var autoStart = true;
 var stage
 var hero
 var life
@@ -5,52 +6,62 @@ var level = 1
 var time
 var co2Niveau = 0
 var preloadText
-
+var muteButton;
 
 function init(){
     stage = new createjs.Stage("canvas");
+    preloadText = new createjs.Text("", "50px Arial", "#000");
+    stage.addChild(preloadText);
+    preloadText.textBaseline="middle";
+    preloadText.textAlign="center";
+    preloadText.x=stage.canvas.width/2;
+    preloadText.y=stage.canvas.height/2;
 
+    preload()
+}
 
-    queue = new createjs.LoadQueue(true);
-    preloadText = new createjs.Text("", "50px Courier New", "#000");
+function preload(){
+    var queue = new createjs.LoadQueue(autoStart);
     queue.installPlugin(createjs.Sound);
     queue.on("progress", queueProgress);
     queue.on("complete", queueComplete);
     queue.loadManifest([
-        //"img/sprites/tiles.png",
-        //"img/hero.png",
-        //"img/key.png",
-        //{id:"tileSprites",src:"json/bgtiles.json"},
-        //{id:"levelJson",src:"json/levels/levels.json"},
-        {id:"bgsound", src:"sounds/bass.mp3"},
-
+        "img/bigPic.jpg",
+        {id:"levelJson",src:"json/levels.json"},
+        {id:"bgsound", src:"sounds/bass.mp3"}
     ])
-
-    hero = new createjs.Shape();
-    hero.graphics.beginFill("blue");
-    hero.graphics.drawCircle(0, 0, 40);
-    hero.x = 50;
-    hero.y = 50;
-
-
-    stage.addChild(hero);
 
 }
 
 function queueProgress(e){
-    preloadText.text= Math.round(e.progress*100)+"%";
-    console.log("Progress");
-
+    preloadText.text="Loading... "+ Math.round(e.progress*100)+"%";
+    stage.update(e);
 }
 
 function queueComplete(){
     createjs.Ticker.addEventListener("tick", tock);
     createjs.Ticker.setFPS(30);
+    stage.removeChild(preloadText);
+    bgSound();
+    console.log('load complete')
+
+}
+
+function bgSound() {
+    muteButton = new createjs.Shape();
+    muteButton.graphics.beginFill("blue");
+    muteButton.graphics.drawCircle(0, 0, 20);
+    muteButton.x = 50;
+    muteButton.y = 50;
+    stage.addChild(muteButton);
+
+
+
 }
 
 
 
 function tock(e){
     stage.update(e);
-    console.log("hej hej hej")
+    //console.log("tock is running")
 }
