c1684ebb37407b95c213aa6497036eaf15d84708
Mattias Tastum <Mattias Tastum>
2016-05-31 12:14:41 +0200

    Input - levels

--- js/script.js
+++ js/script.js
@@ -7,6 +7,11 @@ var time
 var co2Niveau = 0
 var preloadText
 var muteButton;
+var levelData;
+var tiles;
+var currentLevel=-1;
+var queue;
+var blockSize = 50;
 
 function init(){
     stage = new createjs.Stage("canvas");
@@ -21,13 +26,14 @@ function init(){
 }
 
 function preload(){
-    var queue = new createjs.LoadQueue(autoStart);
+    queue = new createjs.LoadQueue(autoStart);
     queue.installPlugin(createjs.Sound);
     queue.on("progress", queueProgress);
     queue.on("complete", queueComplete);
     queue.loadManifest([
         "img/bigPic.jpg",
         {id:"levelJson",src:"json/levels.json"},
+        {id:"tiles",src:"json/tiles.json"},
         {id:"bgsound", src:"sounds/bass.mp3"}
     ])
 
@@ -44,7 +50,45 @@ function queueComplete(){
     stage.removeChild(preloadText);
     bgSound();
     console.log('load complete')
+    levelData = queue.getResult("levelJson")
+    tiles = new createjs.SpriteSheet(queue.getResult("tiles"));
+    setupLevel();
+}
 
+function setupLevel(){
+    var row, col;
+    currentLevel++;
+    var level = levelData.levels[currentLevel].tiles;
+    //console.log(level);
+    for(row=0; row<level.length; row++){
+        //console.log(level[row]);
+        for(col=0; col<level[row].length; col++){
+          console.log(level[row].length)
+            var img;
+            switch(level[row][col]){
+                case 0:
+                    img = "grass";
+                    break;
+
+                case 1:
+                    img = "block";
+                    break;
+                case 2:
+                    img = "gblock";
+                    break;
+
+                case 3:
+                    img = "windmill";
+                    break;
+            }
+            var t = new createjs.Sprite(tiles, img);
+            t.x=col*blockSize;
+            t.y=row*blockSize;
+            stage.addChild(t);
+        }
+    }
+    /*var t = new createjs.Sprite(tiles, "block");
+    stage.addChild(t);*/
 }
 
 function bgSound() {
